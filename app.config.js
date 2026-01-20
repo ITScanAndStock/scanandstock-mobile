16d79c2ee1e1cc5b232010f20664ad5416220baf
Robin Fantino <robi.fantino.rf@gmail.com>
2026-01-20 10:14:24 +0100

    Refactor app configuration for improved readability and consistency; update app icons

--- app.config.js
+++ app.config.js
@@ -1,123 +1,133 @@
 // app.config.js
-const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+const IS_PREVIEW = process.env.APP_VARIANT === "preview";
 
 // Charger les variables selon le profil de build
 const getEnvVars = () => {
-	// EAS Build définit EXPO_PUBLIC_ENV via eas.json
-	const env = process.env.EXPO_PUBLIC_ENV || 'staging';
+  // EAS Build définit EXPO_PUBLIC_ENV via eas.json
+  const env = process.env.EXPO_PUBLIC_ENV || "staging";
 
-	return {
-		EXPO_PUBLIC_ENV: env,
-		EXPO_PUBLIC_KEYCLOAK_URL: process.env.EXPO_PUBLIC_KEYCLOAK_URL || 'https://staging-sso.myscanandstock.fr/',
-		EXPO_PUBLIC_KEYCLOAK_REALM: process.env.EXPO_PUBLIC_KEYCLOAK_REALM || 'scan-and-stock',
-		EXPO_PUBLIC_KEYCLOAK_CLIENT_ID: process.env.EXPO_PUBLIC_KEYCLOAK_CLIENT_ID || 'scanandstock-mobile',
-		EXPO_PUBLIC_API_URL: process.env.EXPO_PUBLIC_API_URL || 'https://staging-api.myscanandstock.fr/api',
-		EXPO_PUBLIC_COOMPY_URL: process.env.EXPO_PUBLIC_COOMPY_URL || 'https://staging-api.mycoompy.fr/api',
-		EXPO_PUBLIC_MY_COOMPY_URL: process.env.EXPO_PUBLIC_MY_COOMPY_URL || 'https://staging.mycoompy.fr',
-	};
+  return {
+    EXPO_PUBLIC_ENV: env,
+    EXPO_PUBLIC_KEYCLOAK_URL:
+      process.env.EXPO_PUBLIC_KEYCLOAK_URL ||
+      "https://staging-sso.myscanandstock.fr/",
+    EXPO_PUBLIC_KEYCLOAK_REALM:
+      process.env.EXPO_PUBLIC_KEYCLOAK_REALM || "scan-and-stock",
+    EXPO_PUBLIC_KEYCLOAK_CLIENT_ID:
+      process.env.EXPO_PUBLIC_KEYCLOAK_CLIENT_ID || "scanandstock-mobile",
+    EXPO_PUBLIC_API_URL:
+      process.env.EXPO_PUBLIC_API_URL ||
+      "https://staging-api.myscanandstock.fr/api",
+    EXPO_PUBLIC_COOMPY_URL:
+      process.env.EXPO_PUBLIC_COOMPY_URL ||
+      "https://staging-api.mycoompy.fr/api",
+    EXPO_PUBLIC_MY_COOMPY_URL:
+      process.env.EXPO_PUBLIC_MY_COOMPY_URL || "https://staging.mycoompy.fr",
+  };
 };
 
 const envVars = getEnvVars();
 
 export default {
-	expo: {
-		name: IS_PREVIEW ? 'ScanAndStock (Staging)' : 'ScanAndStock',
-		slug: 'scanandstock',
-		version: '1.0.0',
-		orientation: 'portrait',
-		icon: './assets/images/splash-icon.png',
-		scheme: 'scanandstock',
-		userInterfaceStyle: 'automatic',
-		newArchEnabled: true,
-		ios: {
-			supportsTablet: true,
-			bundleIdentifier: 'com.scanandstock.mobile', // Changez selon votre nom
-		},
-		android: {
-			package: "com.scanandstock.mobile",
-			adaptiveIcon: {
-				// backgroundColor: '#E6F4FE',
-				foregroundImage: './assets/images/adaptive-icon.png',
-				// backgroundImage: './assets/images/splash-icon.png',
-				monochromeImage: './assets/images/adaptive-icon.png',
-			},
-			edgeToEdgeEnabled: true,
-			package: 'com.scanandstock.scanandstock',
-			predictiveBackGestureEnabled: false,
-			intentFilters: [
-				{
-					action: 'VIEW',
-					data: [
-						{
-							scheme: 'scanandstock',
-						},
-					],
-					category: ['BROWSABLE', 'DEFAULT'],
-				},
-				{
-					action: 'VIEW',
-					data: [
-						{
-							scheme: 'scanandstock',
-						},
-					],
-					category: ['BROWSABLE', 'DEFAULT'],
-				},
-			],
-		},
-		web: {
-			output: 'static',
-			favicon: './assets/images/favicon.png',
-		},
-		plugins: [
-			'expo-router',
-			[
-				'expo-camera',
-				{
-					cameraPermission: 'Allow $(PRODUCT_NAME) to access your camera',
-					microphonePermission: 'Allow $(PRODUCT_NAME) to access your microphone',
-					recordAudioAndroid: true,
-				},
-			],
-			[
-				'expo-splash-screen',
-				{
-					image: './assets/images/splash-icon.png',
-					resizeMode: 'contain',
-					backgroundColor: '#ffffff',
-				},
-			],
-			[
-				'expo-secure-store',
-				{
-					faceIDPermission: 'Allow $(PRODUCT_NAME) to access your Face ID biometric data.',
-				},
-			],
-			'expo-audio',
-		],
-		experiments: {
-			typedRoutes: true,
-			reactCompiler: true,
-		},
-		extra: {
-			router: {},
-			eas: {
-				projectId: 'b3291952-eb54-434f-b546-6cac13f8a086',
-			},
-			// Injecter les variables d'environnement dans extra
-			env: envVars.EXPO_PUBLIC_ENV,
-			KEYCLOAK_URL: envVars.EXPO_PUBLIC_KEYCLOAK_URL,
-			KEYCLOAK_REALM: envVars.EXPO_PUBLIC_KEYCLOAK_REALM,
-			KEYCLOAK_CLIENT_ID: envVars.EXPO_PUBLIC_KEYCLOAK_CLIENT_ID,
-			API_URL: envVars.EXPO_PUBLIC_API_URL,
-			COOMPY_URL: envVars.EXPO_PUBLIC_COOMPY_URL,
-			MY_COOMPY_URL: envVars.EXPO_PUBLIC_MY_COOMPY_URL,
-		},
-		runtimeVersion: {
-			policy: 'appVersion',
-		},
-		updates: {
-			url: 'https://u.expo.dev/b3291952-eb54-434f-b546-6cac13f8a086',
-		},
-	},
+  expo: {
+    name: IS_PREVIEW ? "ScanAndStock (Staging)" : "Scan&Stock",
+    slug: "scanandstock",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/splash-icon.png",
+    scheme: "scanandstock",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.scanandstock.mobile",
+      icon: "./assets/images/ios-app-icon.png",
+    },
+    android: {
+      package: "com.scanandstock.mobile",
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/android-app-icon.png",
+        monochromeImage: "./assets/images/android-app-icon.png",
+      },
+      edgeToEdgeEnabled: true,
+      package: "com.scanandstock.scanandstock",
+      predictiveBackGestureEnabled: false,
+      intentFilters: [
+        {
+          action: "VIEW",
+          data: [
+            {
+              scheme: "scanandstock",
+            },
+          ],
+          category: ["BROWSABLE", "DEFAULT"],
+        },
+        {
+          action: "VIEW",
+          data: [
+            {
+              scheme: "scanandstock",
+            },
+          ],
+          category: ["BROWSABLE", "DEFAULT"],
+        },
+      ],
+    },
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-camera",
+        {
+          cameraPermission: "Allow $(PRODUCT_NAME) to access your camera",
+          microphonePermission:
+            "Allow $(PRODUCT_NAME) to access your microphone",
+          recordAudioAndroid: true,
+        },
+      ],
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+      [
+        "expo-secure-store",
+        {
+          faceIDPermission:
+            "Allow $(PRODUCT_NAME) to access your Face ID biometric data.",
+        },
+      ],
+      "expo-audio",
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "b3291952-eb54-434f-b546-6cac13f8a086",
+      },
+      // Injecter les variables d'environnement dans extra
+      env: envVars.EXPO_PUBLIC_ENV,
+      KEYCLOAK_URL: envVars.EXPO_PUBLIC_KEYCLOAK_URL,
+      KEYCLOAK_REALM: envVars.EXPO_PUBLIC_KEYCLOAK_REALM,
+      KEYCLOAK_CLIENT_ID: envVars.EXPO_PUBLIC_KEYCLOAK_CLIENT_ID,
+      API_URL: envVars.EXPO_PUBLIC_API_URL,
+      COOMPY_URL: envVars.EXPO_PUBLIC_COOMPY_URL,
+      MY_COOMPY_URL: envVars.EXPO_PUBLIC_MY_COOMPY_URL,
+    },
+    runtimeVersion: {
+      policy: "appVersion",
+    },
+    updates: {
+      url: "https://u.expo.dev/b3291952-eb54-434f-b546-6cac13f8a086",
+    },
+  },
 };
